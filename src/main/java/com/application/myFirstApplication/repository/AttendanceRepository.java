00b7fb7869c2d09610c92fc9f5d5be99c2ccd8c5
KristinaKuzmic <kristinakuzmic52@gmail.com>
2024-09-27 19:22:06 +0100

    Commit

--- src/main/java/com/application/myFirstApplication/repository/AttendanceRepository.java
+++ src/main/java/com/application/myFirstApplication/repository/AttendanceRepository.java
@@ -1,9 +1,9 @@
 package com.application.myFirstApplication.repository;
 
-import com.application.myFirstApplication.model.Course;
+import com.application.myFirstApplication.model.Attendance;
 import org.springframework.data.jpa.repository.JpaRepository;
 import org.springframework.stereotype.Repository;
 
 @Repository
-public interface CourseRepository extends JpaRepository<Course, Integer> {
+public interface AttendanceRepository extends JpaRepository<Attendance, Integer> {
 }
